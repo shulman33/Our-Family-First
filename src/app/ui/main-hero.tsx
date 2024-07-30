7d4690b89076343046b85d563c8ebcd88a9a0fb2
Sam Shulman <sshulma5@mail.yu.edu>
2024-07-30 18:16:01 -0400

    refactored donate to register and changed route /donate => /aug6

--- src/app/ui/main-hero.tsx
+++ src/app/ui/main-hero.tsx
@@ -17,10 +17,10 @@ return (
                 </p>
                 <div className="mt-10 flex items-center gap-x-6">
                     <Link
-                        href="/donate"
+                        href="/aug6"
                         className="rounded-md bg-customBlue px-8 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                        Donate Now
+                        Register Now
                     </Link>
                 </div>
             </div>
